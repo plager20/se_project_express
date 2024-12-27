259393b7e1623797b1fe1c9cf54665e6f8c967ca
Gerald Planty <plantygerald02@gmail.com>
2024-12-27 10:59:18 -0500

    Users Completed

--- controllers/users.js
+++ controllers/users.js
@@ -18,8 +18,27 @@ const createUser = (req, res) => {
     .then((user) => res.status(201).send(user))
     .catch((err) => {
       console.error(err);
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: err.message });
+      }
       return res.status(500).send({ message: err.message });
     });
 };
 
-module.exports = { getUsers, createUser };
+const getUser = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .orFail()
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: err.message });
+      } else if (err.name === "CastError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = { getUsers, createUser, getUser };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-const { getUsers, createUser } = require("../controllers/users");
+const { getUsers, createUser, getUser } = require("../controllers/users");
 
 router.get("/", getUsers);
-// router.get("/:userId", () => console.log("getUser"));
+router.get("/:userId", getUser);
 router.post("/", createUser);
 
 module.exports = router;
