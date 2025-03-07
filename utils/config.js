bae57f39334e7cfcef34c569d09e968f0feccf0f
Gerald Planty <plantygerald02@gmail.com>
2025-03-07 17:52:36 -0500

    env variables

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,7 @@
+const { JWT_SECRET = "super-strong-secret" } = process.env;
+
 module.exports = {
-  JWT_SECRET: "super-secret-pass",
+  JWT_SECRET,
 };
+
+// 8b0add56fe02e0ba6d0cd659750bd4a6e00611e2fa5976474102f1ec5b663503
